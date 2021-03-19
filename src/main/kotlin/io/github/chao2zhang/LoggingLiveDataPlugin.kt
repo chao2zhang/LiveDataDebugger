331d00474387f415d892a2dc88b93b552b6315bd
Chao Zhang <cazhang@linkedin.com>
2021-03-19 15:10:22 -0700

    Create an empty plugin wrapper

---
+++ src/main/kotlin/io/github/chao2zhang/LoggingLiveDataPlugin.kt
@@ -0,0 +1,11 @@
+package io.github.chao2zhang
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LoggingLiveDataPlugin : Plugin<Project> {
+
+    override fun apply(p0: Project) {
+        TODO("Not yet implemented")
+    }
+}
